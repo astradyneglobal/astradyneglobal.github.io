c74633323c61a257654e5ec6d9563bd451912971
astradyneglobal <astradyne.global@gmail.com>
2025-06-30 12:10:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+<script>
+        function redirectWithDelay() {
+            const delay = 3000; // Delay in milliseconds (3 seconds)
+            setTimeout(() => {
+                window.location.href = '/index2.html'; // Redirect URL
+            }, delay);
+        }
+</script>
